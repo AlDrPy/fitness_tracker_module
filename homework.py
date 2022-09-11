a9ff3a371a92a7202b84cb6db459f089509432b1
Александр Дробязко <al.drobias@yandex.ru>
2022-09-11 22:50:02 +0300

    Child classes described, gitignore file modified

--- homework.py
+++ homework.py
@@ -44,17 +44,61 @@ class Training:
 
 class Running(Training):
     """Тренировка: бег."""
-    pass
+
+    def get_spent_calories(self) -> float:
+        coeff_calorie_1 = 18
+        coeff_calorie_2 = 20
+        return ((coeff_calorie_1 * self.get_mean_speed() - coeff_calorie_2) 
+                * self.weight / Training.M_IN_KM * self.duration)
 
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
-    pass
+
+    def __init__(self,
+                action: int,
+                duration: float,
+                weight: float,
+                height: float,
+                ) -> None:
+        super().__init__(action, duration, weight)
+        self.height = height
+
+    def get_spent_calories(self) -> float:
+        """Получить количество затраченных калорий."""
+        coeff_calorie_1 = 0.035
+        coeff_calorie_2 = 0.029
+        return ((coeff_calorie_1 * self.weight + (self.get_mean_speed()**2
+                // self.height) * coeff_calorie_2 * self.weight) 
+                * self.duration)
 
 
 class Swimming(Training):
     """Тренировка: плавание."""
-    pass
+
+    LEN_STEP: float = 1.38
+    def __init__(self,
+                action: int,
+                duration: float,
+                weight: float,
+                length_pool: int,
+                count_pool: int,
+                ) -> None:
+        super().__init__(action, duration, weight)
+        self.length_pool = length_pool
+        self.count_pool = count_pool
+
+    def get_mean_speed(self) -> float:
+        """Получить среднюю скорость движения."""
+        return (self.length_pool * self.count_pool 
+                / Training.M_IN_KM / self.duration)
+
+    def get_spent_calories(self) -> float:
+        """Получить количество затраченных калорий."""
+        coeff_calorie_1 = 1.1
+        coeff_calorie_2 = 2
+        return ((self.get_mean_speed() + coeff_calorie_1) 
+                * coeff_calorie_2 * self.weight) 
 
 
 def read_package(workout_type: str, data: list) -> Training:
@@ -68,12 +112,17 @@ def main(training: Training) -> None:
 
 
 if __name__ == '__main__':
-    packages = [
-        ('SWM', [720, 1, 80, 25, 40]),
-        ('RUN', [15000, 1, 75]),
-        ('WLK', [9000, 1, 75, 180]),
-    ]
+    # packages = [
+    #     ('SWM', [720, 1, 80, 25, 40]),
+    #     ('RUN', [15000, 1, 75]),
+    #     ('WLK', [9000, 1, 75, 180]),
+    # ]
 
-    for workout_type, data in packages:
-        training = read_package(workout_type, data)
-        main(training)
+    # for workout_type, data in packages:
+    #     training = read_package(workout_type, data)
+    #     main(training)
+    print(Training.LEN_STEP)
+    run1 = Running(15000, 1, 75)
+    print(run1.get_distance())
+    print(run1.get_mean_speed())
+    print(run1.get_spent_calories())
